c13fb21056cbf48bdc88dc857d35c1a1898f2719
locks <rokusu@gmail.com>
2009-03-31 19:45:41 +0100

    refactorizacao do InterfaceTexto

--- src/pprogsmt/Main.java
+++ src/pprogsmt/Main.java
@@ -11,38 +11,37 @@ public class Main {
      * @throws java.lang.Exception
      */
     public static void main(final String[] args) throws Exception {
-        Sistema sistema = new Sistema();
-
+        InterfaceTexto sistema = new InterfaceTexto();
+        
+        // teste
         sistema.criarConta("Joao","");
         sistema.criarConta("Tiago","");
         sistema.criarConta("teste", "teste");
-
-        InterfaceTexto.ecraInicial(sistema);
-
-        Class.forName("org.sqlite.JDBC");
-        Connection conn = DriverManager.getConnection("jdbc:sqlite:test.db");
-
-        Statement stat = conn.createStatement();
-        stat.executeUpdate("drop table if exists Utilizador;");
-        stat.executeUpdate("create table Utilizador (nome, password);");
-        PreparedStatement prep = conn.prepareStatement("insert into Utilizador values (?, ?);");
-
-        for (int i=0; i<sistema.getSize(); i++) {
-            prep.setString(1, sistema.getUtilizador(i).getNome());
-            prep.setString(2, sistema.getUtilizador(i).getPassword());
-            prep.addBatch();
-        }
-
-        conn.setAutoCommit(false);
-        prep.executeBatch();
-        conn.setAutoCommit(true);
-
-        ResultSet rs = stat.executeQuery("select * from Utilizador;");
-        while (rs.next()) {
-            System.out.println("nome: " + rs.getString("nome"));
-            System.out.println("password = " + rs.getString("password"));
-        }
-        rs.close();
-        conn.close();
+        // teste
+//        Class.forName("org.sqlite.JDBC");
+//        Connection conn = DriverManager.getConnection("jdbc:sqlite:test.db");
+//
+//        Statement stat = conn.createStatement();
+//        stat.executeUpdate("drop table if exists Utilizador;");
+//        stat.executeUpdate("create table Utilizador (nome, password);");
+//        PreparedStatement prep = conn.prepareStatement("insert into Utilizador values (?, ?);");
+//
+//        for (int i=0; i<sistema.getSize(); i++) {
+//            prep.setString(1, sistema.getUtilizador(i).getNome());
+//            prep.setString(2, sistema.getUtilizador(i).getPassword());
+//            prep.addBatch();
+//        }
+//
+//        conn.setAutoCommit(false);
+//        prep.executeBatch();
+//        conn.setAutoCommit(true);
+//
+//        ResultSet rs = stat.executeQuery("select * from Utilizador;");
+//        while (rs.next()) {
+//            System.out.println("nome: " + rs.getString("nome"));
+//            System.out.println("password = " + rs.getString("password"));
+//        }
+//        rs.close();
+//        conn.close();
     }
 }
