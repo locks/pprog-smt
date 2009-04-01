ee4ca5f45dbed18702ed13df5bfe489a128d575c
lockstheadmin <lockstheadmin@ikaruga>
2009-04-01 19:39:26 +0100

    tirei os comentários

--- src/ClassesTeste/CaixaDeMensagens.java
+++ src/ClassesTeste/CaixaDeMensagens.java
@@ -5,9 +5,6 @@ import java.util.ArrayList;
 public class CaixaDeMensagens {
     private ArrayList<Mensagem> mensagens;
 
-    /**
-     *
-     */
     public CaixaDeMensagens() {
         mensagens = new ArrayList<Mensagem>();
     }
