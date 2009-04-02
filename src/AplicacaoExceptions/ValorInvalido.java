c1bf4423ec3935ddbe9411c140cb9bfd056d8771
locks <rokusu@gmail.com>
2009-04-02 03:44:31 +0100

    comentarios boilerplate é uma chatice

--- src/AplicacaoExceptions/ValorInvalido.java
+++ src/AplicacaoExceptions/ValorInvalido.java
@@ -1,14 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package aplicacaoexceptions;
 
-/**
- *
- * @author locks
- */
 public class ValorInvalido extends Exception {
     public ValorInvalido() {
         super("Valor introduzido é inválido.");
